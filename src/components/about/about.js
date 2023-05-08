decfcca83168a6b7f9d95bc2d94880f9b2b4fab7
MoriahSWalker git config --global user.email riahrosebuds@gmail.com <you@example.com>
2023-05-08 18:34:33 -0400

    update to  about me section

--- src/components/about/about.js
+++ src/components/about/about.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./about.css";
-import Me from "../../assets/mockphoto.jpeg";
+import Framework from "../../assets/framwork_photo.avif";
 import { GiSkills } from "react-icons/gi";
 import { TbCertificate } from "react-icons/tb";
 import { BiBookReader } from "react-icons/bi";
@@ -13,7 +13,7 @@ const About = () => {
       <div className="container about__container">
         <div className="about__me">
           <div className="about__me-image">
-            <img src={Me} alt="about image"></img>
+            <img src={Framework} alt="framework photo"></img>
           </div>
         </div>
         <div className="about__content">
@@ -21,7 +21,7 @@ const About = () => {
             <article className="about__card">
               <GiSkills className="tools__icon" />
               <h5>Tool Kit</h5>
-              <small>MERN CSS HTML JAVASCRIPT</small>
+              <small>MERN CSS HTML JAVASCRIPT BOOTSTRAP</small>
             </article>
 
             <article className="about__card">
@@ -32,11 +32,16 @@ const About = () => {
 
             <article className="about__card">
               <BiBookReader className="portfolio__icon" />
-              <h5>Portfolio</h5>
-              <small>See My Work</small>
+              <h5>Design Stack</h5>
+              <small>Figma InDesign Photoshop Illustrator</small>
             </article>
           </div>
-          <p>This is going to be a summary paragraph</p>
+          <p>
+            I am a Freelance UI/UX Designer and Full Stack Developer. I am a
+            fanatic for artistic design and branding. I studied graphic design
+            at Full Sail University, then went on to earn a certificate for
+            software engineering at Perscholas.
+          </p>
           <a href="#contact" className="btn btn-primary">
             Work Together
           </a>
